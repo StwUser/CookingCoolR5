c129385d319b043f1b522e818f8b367f153e7f6b
Stw <47381003+StwUser@users.noreply.github.com>
2023-07-05 22:53:59 +0300

    changeImg

--- CookingCoolR5/client-app/src/components/navigation/Navigation.tsx
+++ CookingCoolR5/client-app/src/components/navigation/Navigation.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Navigation.css";
+import StoreIcon from "../../img/store-icon3.png"
 
 function Navigation(): JSX.Element {
 
     return (
         <div className="Content-row-header">
-            <div className="Nav-item">1</div>
+            <div className="Nav-item Logo-item">
+                <div className="Column-div">
+                    <div>
+                        <img src={StoreIcon} className="Store-icon" alt="StoreIcon" />
+                        <span className="Logo-text">Game Stores</span>
+                    </div>
+                    <div>seeds</div>
+                </div>
+            </div>
             <div className="Nav-item">2</div>
             <div className="Nav-item">3</div>
             <div className="Nav-item">4</div>
