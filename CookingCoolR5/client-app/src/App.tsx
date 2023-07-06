a34f7ccddbdf692ac1456b92d55a385c78426992
Stw <47381003+StwUser@users.noreply.github.com>
2023-07-06 13:20:52 +0300

    maybe

--- CookingCoolR5/client-app/src/App.tsx
+++ CookingCoolR5/client-app/src/App.tsx
@@ -3,22 +3,21 @@ import './App.css';
 import { IUser } from './services/AuthService';
 import LoginForm from './components/loginform/LoginForm';
 import ContentForm from './components/contentform/ContentForm';
-import { sortAndDeduplicateDiagnostics } from 'typescript';
 
 function App() {
   const [user, setUser] = useState<IUser | null>(null);
 
-  const setUpCss = () => {
-    const appDiv = document.getElementById('Application');
-    if (user !== null) {
-      appDiv!.style.justifyContent = 'start';
-    }
-    else {
-      appDiv!.style.justifyContent = 'center';
-    }
-  }
-
   useEffect(() => {
+
+    const setUpCss = () => {
+      const appDiv = document.getElementById('Application');
+      if (user !== null) {
+        appDiv!.style.justifyContent = 'start';
+      }
+      else {
+        appDiv!.style.justifyContent = 'center';
+      }
+    }
 
     setUpCss();
 
