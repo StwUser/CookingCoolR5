7bde3547f4ce17be782b72edd4ebe130fc1647ab
Stw <47381003+StwUser@users.noreply.github.com>
2023-07-20 20:33:01 +0300

    bbq

--- CookingCoolR5/client-app/src/components/gameitem/GameItem.tsx
+++ CookingCoolR5/client-app/src/components/gameitem/GameItem.tsx
@@ -24,12 +24,15 @@ function GameItem({game}: IGameItem): JSX.Element {
         <div key={game?.id} className="Game-item">
             <div className="Store-div">
                 <span className="Store-l">{game?.site}</span>
+                <div className="Free-space-h"></div>
                 {isGog && <img src={GogI} className="Store-img-c" alt="GOG"></img>}
                 {isSteam && <img src={SteamI} className="Store-img-c" alt="Steam"></img>}
                 {isEpic && <img src={EpicI} className="Store-img-c" alt="Epic Games"></img>}
             </div>
             <div className="Store-game-n">{game?.name}</div>
             <img src={game?.image} className="Game-img-c" alt={game?.name} />
+            <div className="Image-free-space">
+            </div>
             <div><span className="Old-price">{game?.priceWithoutDiscount}</span><span className="Discount-p">{game?.discount}</span></div>
             <div className="Price-now">{game?.price}</div>
             <a href={game?.href} target="_blank" rel="noreferrer" className="Site-page">Site page</a>
